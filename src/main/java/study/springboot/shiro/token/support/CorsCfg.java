821bd2ad6ec92e21ab2c7eeda2286a71a400d2fa
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-05-29 13:42:38 +0800

    提交代码

--- src/main/java/study/springboot/shiro/token/support/CorsCfg.java
+++ src/main/java/study/springboot/shiro/token/support/CorsCfg.java
@@ -13,11 +13,11 @@ public class CorsCfg {
     @Bean
     public CorsConfiguration buildConfig() {
         CorsConfiguration corsCfg = new CorsConfiguration();
-        // 允许任何域名使用
+        //允许任何域名使用
         corsCfg.addAllowedOrigin("*");
-        // 允许任何头
+        //允许任何头
         corsCfg.addAllowedHeader("*");
-        // 允许任何方法（post、get等）
+        //允许任何方法（post、get等）
         corsCfg.addAllowedMethod("*");
         return corsCfg;
     }
@@ -26,7 +26,7 @@ public class CorsCfg {
     @Bean
     public CorsFilter corsFilter(CorsConfiguration corsCfg) {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        // 对接口配置跨域设置
+        //对接口配置跨域设置
         source.registerCorsConfiguration("/**", corsCfg);
         return new CorsFilter((CorsConfigurationSource) source);
     }
