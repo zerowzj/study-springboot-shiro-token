916f853e8d0f3c6204e2a0a3dffaaf489a6f8e57
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-05-29 23:15:00 +0800

    提交代码

--- src/main/java/study/springboot/shiro/token/support/shiro/ShiroCfg.java
+++ src/main/java/study/springboot/shiro/token/support/shiro/ShiroCfg.java
@@ -1,6 +1,5 @@
-package study.springboot.shiro.token.auth;
+package study.springboot.shiro.token.support.shiro;
 
-import com.google.common.collect.Maps;
 import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
 import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.mgt.SecurityManager;
@@ -18,6 +17,7 @@ import study.springboot.shiro.token.auth.realm.CustomRealm;
 import study.springboot.shiro.token.auth.subject.CustomSubjectFactory;
 
 import javax.servlet.Filter;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Configuration
@@ -26,11 +26,25 @@ public class ShiroCfg {
     @Autowired
     private CustomRealm customRealm;
     @Autowired
-    private CustomSubjectFactory subjectFactory;
+    private CustomSubjectFactory customSubjectFactory;
     @Autowired
     private RedisCacheManager redisCacheManager;
     @Autowired
     private CustomAuthFilter customAuthFilter;
+//    @Bean
+//    public CustomSubjectFactory customSubjectFactory(){
+//        return new CustomSubjectFactory();
+//    }
+//
+//    @Bean
+//    public CustomRealm customRealm(){
+//        return new CustomRealm();
+//    }
+//    @Bean
+//    public CustomAuthFilter customAuthFilter(){
+//        return new CustomAuthFilter();
+//    }
+
 
     /**
      * SessionManager通过sessionValidationSchedulerEnabled禁用掉会话调度器
@@ -51,7 +65,7 @@ public class ShiroCfg {
         //（▲）Realm
         manager.setRealm(customRealm);
         //（▲）Subject工厂
-        manager.setSubjectFactory(subjectFactory);
+        manager.setSubjectFactory(customSubjectFactory);
         //（▲）禁用Session作为存储策略的实现
         DefaultSubjectDAO subjectDAO = (DefaultSubjectDAO) manager.getSubjectDAO();
         DefaultSessionStorageEvaluator storageEvaluator = (DefaultSessionStorageEvaluator) subjectDAO.getSessionStorageEvaluator();
@@ -69,35 +83,34 @@ public class ShiroCfg {
         ShiroFilterFactoryBean factoryBean = new ShiroFilterFactoryBean();
         //（▲）安全管理器
         factoryBean.setSecurityManager(securityManager);
-        //（▲）
+        //（▲）过滤器
+        Map<String, Filter> filterMap = factoryBean.getFilters();
+        filterMap.put("authc", customAuthFilter);
+        //（▲）登录跳转
         factoryBean.setSuccessUrl("/welcome");           //认证成功
         factoryBean.setLoginUrl("/unauthorized");        //未认证
         factoryBean.setUnauthorizedUrl("/unauthorized"); //未授权
-        //（▲）设置规则，使用LinkedHashMap，因为拦截有先后顺序
-        Map<String, String> filterChainDefinition = Maps.newLinkedHashMap();
+        //（▲）设置规则
+        //使用LinkedHashMap，因为拦截有先后顺序
+        Map<String, String> filterChainDefinition = new LinkedHashMap();
         //登录接口不需要认证
         filterChainDefinition.put("/login", "anon");
         //其他资源地址全部需要通过代理登录步骤，注意顺序，必须先进过无状态代理登录后，后面的权限和角色认证才能使用
-        filterChainDefinition.put("/**", "statelessAuthc");
+        filterChainDefinition.put("/**", "authc");
         factoryBean.setFilterChainDefinitionMap(filterChainDefinition);
-        //（▲）过滤器
-        Map<String, Filter> filterMap = factoryBean.getFilters();
-        filterMap.put("statelessAuthc", customAuthFilter);
+
         return factoryBean;
     }
-//
+
 //    @Bean("lifecycleBeanPostProcessor")
 //    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
 //        return new LifecycleBeanPostProcessor();
 //    }
-
-    @Bean
-    public FilterRegistrationBean delegatingFilterProxy() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        DelegatingFilterProxy proxy = new DelegatingFilterProxy();
-        proxy.setTargetFilterLifecycle(true);
-        proxy.setTargetBeanName("shiroFilter");
-        filterRegistrationBean.setFilter(proxy);
-        return filterRegistrationBean;
-    }
+//
+//    @Bean
+//    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+//        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+//        proxyCreator.setProxyTargetClass(true);
+//        return proxyCreator;
+//    }
 }
---
+++ src/main/java/study/springboot/shiro/token/support/shiro/ShiroFilterCfg.java
@@ -0,0 +1,26 @@
+package study.springboot.shiro.token.support.shiro;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import study.springboot.shiro.token.auth.filter.CustomAuthFilter;
+
+@Component
+public class ShiroFilterCfg {
+
+    @Bean
+    public FilterRegistrationBean shiroLoginFilteRegistration(CustomAuthFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean(filter);
+        registration.setEnabled(false);
+        return registration;
+    }
+
+//    @Bean
+//    public FilterRegistrationBean delegatingFilterProxy() {
+//        DelegatingFilterProxy proxy = new DelegatingFilterProxy();
+//        proxy.setTargetFilterLifecycle(true);
+//        proxy.setTargetBeanName("shiroFilter");
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(proxy);
+//        return filterRegistrationBean;
+//    }
+}
