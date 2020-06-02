d727e58f77bf787d85a6c0393a6f02986bf83a29
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-06-02 15:21:14 +0800

    提交代码

--- src/main/java/study/springboot/shiro/token/support/redis/RedisClient.java
+++ src/main/java/study/springboot/shiro/token/support/redis/RedisClient.java
@@ -4,21 +4,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class RedisClient {
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
-    public String get(String key) {
-        return redisTemplate.opsForValue().get(key);
-    }
-
+    /**
+     *
+     */
     public void set(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
     }
 
-    public void set(String key, Object value) {
-        redisTemplate.opsForValue().set(key, "");
+    public void set(String key, String value, long expire) {
+        redisTemplate.opsForValue()
+                .set(key, value, expire, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * 获取string
+     */
+    public String get(String key) {
+        Object obj = redisTemplate.opsForValue().get(key);
+        String value = null;
+        if (obj != null) {
+            value = obj.toString();
+        }
+        return value;
+    }
+
+    /**
+     * key存在
+     */
+    public boolean hasKey(String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * key过期
+     */
+    public boolean expire(String key, long expire) {
+        return redisTemplate.expire(key, expire, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * key删除
+     */
+    public boolean delete(String key) {
+        return redisTemplate.delete(key);
     }
 }
---
+++ src/main/java/study/springboot/shiro/token/support/redis/RedisKeys.java
@@ -0,0 +1,22 @@
+package study.springboot.shiro.token.support.redis;
+
+import com.google.common.base.Joiner;
+
+public class RedisKeys {
+
+    private static final String SEPARATOR = ":";
+
+    private static final String APP = "mgr_web";
+
+    private static String format(String... key) {
+        return Joiner.on(SEPARATOR).skipNulls().join(key);
+    }
+
+    public static String keyOfToken(String token) {
+        return format(APP, "token", token);
+    }
+
+    public static String keyOfUserInfo(String email) {
+        return format(APP, "usr_info", email);
+    }
+}
