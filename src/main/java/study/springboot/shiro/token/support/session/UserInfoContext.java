0890ab6a5632881815380bc15c0094b6678b93a9
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-06-02 16:59:02 +0800

    提交代码

--- src/main/java/study/springboot/shiro/token/support/session/UserInfoContext.java
+++ src/main/java/study/springboot/shiro/token/support/session/UserInfoContext.java
@@ -4,14 +4,23 @@ public class UserInfoContext {
 
     private static ThreadLocal<UserInfo> LOCAL = new ThreadLocal<>();
 
+    /**
+     * 获取用户信息
+     */
     public static UserInfo get() {
         return LOCAL.get();
     }
 
+    /**
+     * 保存用户信息
+     */
     public static void set(UserInfo userInfo) {
         LOCAL.set(userInfo);
     }
 
+    /**
+     * 清除用户信息
+     */
     public static void remove() {
         LOCAL.remove();
     }
